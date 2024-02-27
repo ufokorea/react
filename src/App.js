57e8b82e23ca550627af72653096497797d5d366
ufokorea <seetheblue@naver.com>
2024-02-27 20:20:00 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  let counter = 0;  
+  const [counter2,setCounter2]=useState(0);
+  const increase =() =>{
+    counter = counter + 1;
+    setCounter2(counter2 + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{counter}</div>
+      <div>states : {counter2}</div>      
+      <button onClick={increase}>클릭</button>         
     </div>
   );
 }
